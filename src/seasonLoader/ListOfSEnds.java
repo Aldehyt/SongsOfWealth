1fde3d4a838db25bd697d5190ed3b9f6dc81e08c
lerin <aldehyt@gmail.com>
2023-03-24 16:39:05 +0100

    rework to singleton, but still bugged - doJob is called twice

--- src/seasonLoader/ListOfSEnds.java
+++ src/seasonLoader/ListOfSEnds.java
@@ -2,8 +2,9 @@ package seasonLoader;
 
 import java.util.ArrayList;
 
-public class ListOfSEnds {
-    ArrayList<ISEnds> listOfSEnds = new ArrayList<>();
+public class ListOfSEndsManipulator {
+
+    public static final ArrayList<ISEnds> listOfSEnds = new ArrayList<>();
 
     public void executeOverList(){
         for(ISEnds element : listOfSEnds){
