299c60b5bb8229da7a7fed6b1f8047e88c44c1ba
lerin <aldehyt@gmail.com>
2023-03-28 12:40:50 +0200

    Remove that observable, simplification, change seasonTimeChecker to old concept >3.99, add DTO and resources price calculations

--- src/utils/patterns/observer/ISubscriber.java
+++
@@ -1,5 +0,0 @@
-package utils.patterns.observer;
-
-public interface ISubscriber {
-    void notified();
-}
--- src/utils/patterns/observer/Subscriber.java
+++
@@ -1,9 +0,0 @@
-package utils.patterns.observer;
-
-public class Subscriber implements ISubscriber{
-
-    @Override
-    public void notified() {
-
-    }
-}
